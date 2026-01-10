aec819303a338a1b482ed6e947b4e097963967f0
Boppitystar <stephaniechang966@gmail.com>
2026-01-09 22:45:06 -0700

    constants names updated

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,14 +87,14 @@ public final class Constants {
     public static final double Y_TAG_ALIGNMENT_P = 0.1; 
     public static final double ROT_TAG_ALIGNMENT_P = 0.1; 
 
-    public static final double ROT_SETPOINT_REEF_ALIGNMENT = 0;  // Rotation
-    public static final double ROT_TOLERANCE_REEF_ALIGNMENT = 1;
+    public static final double ROT_SETPOINT_TAG_ALIGNMENT = 0;  // Rotation
+    public static final double ROT_TOLERANCE_TAG_ALIGNMENT = 1;
     
-    public static final double X_SETPOINT_REEF_ALIGNMENT = -0.34;  // Vertical pose and tolerance 
-    public static final double X_TOLERANCE_REEF_ALIGNMENT = 0.02;
+    public static final double X_SETPOINT_TAG_ALIGNMENT = -0.34;  // Vertical pose and tolerance 
+    public static final double X_TOLERANCE_TAG_ALIGNMENT = 0.02;
 
-    public static final double Y_SETPOINT_REEF_ALIGNMENT = 0.16;  // Horizontal pose (- for diff sides of tag)
-    public static final double Y_TOLERANCE_REEF_ALIGNMENT = 0.02; 
+    public static final double Y_SETPOINT_TAG_ALIGNMENT = 0.16;  // Horizontal pose (- for diff sides of tag)
+    public static final double Y_TOLERANCE_TAG_ALIGNMENT = 0.02; 
 
     public static final double DONT_SEE_TAG_WAIT_TIME = 1;
 	  public static final double POSE_VALIDATION_TIME = 0.3;
